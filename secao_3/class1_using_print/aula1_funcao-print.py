26bec056a3f1f1abd7c76e21852fb3831b8363c8
Vinicius7Lima <vynnhydossantos@gmail.com>
2023-04-01 14:06:12 -0300

    aulas sobre print e strings

---
+++ secao_3/class1_using_print/aula1_funcao-print.py
@@ -0,0 +1,8 @@
+# Função print: Ela vai exibir uma mensagem para o usuário ou na tela,
+# Ela recebe qualquer tipo de argumento ou mensagem
+# sendo utilizada assim: 
+oi = 'oi'
+print('Olá, seja bem vindo!!!', end='-')
+print(123456, end='**\n')
+print("10 + 10 = 20")
+print(45, 34, 89, sep=f'{oi}')
---
+++ secao_3/class2_tipos_de_dados/aula2-tipo_str.py
@@ -0,0 +1,16 @@
+"""
+Python = Linguagem de programação
+Tipo de tipagem = Dinâminca / Forte
+str -> string = texto
+Strings são textos que estão dentro de aspas
+"""
+
+# aspas simples
+print('Oi')
+# aspas duplas
+print("Teste")
+# Escape
+print("Teste \"Deu certo\"")
+# r 
+print(r'segundo \"comando\"')
+
