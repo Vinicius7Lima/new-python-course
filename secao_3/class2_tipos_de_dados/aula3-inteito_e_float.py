37ac52f9dd3ab2924d5d15bad835363d44caa545
Vinicius7Lima <vynnhydossantos@gmail.com>
2023-04-12 23:53:37 -0300

    aula 3 tipos primitivo

---
+++ secao_3/class2_tipos_de_dados/aula3-inteito_e_float.py
@@ -0,0 +1,16 @@
+# Tipos primitivos -> Inteiro e Float
+# int -> Número inteiro: representa qualquer número inteiro.
+print(11)
+# float -> Número com ponto flutuante: reprenta qualquer número com
+# ponto flutuante.
+# A função type mostra o tipo que o python inferiu ao valor.
+print(4.7)
+print(3.0)
+#
+#
+print(type(42.6))
+print(type(4))
+print(type('Cafezinho'))
+#
+#
+#
